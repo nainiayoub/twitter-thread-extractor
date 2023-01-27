3b8e95d62f233401c77f40829a5d7b614bf500a0
Ayoub NAINIA <nainiaayoub6@gmail.com>
2023-01-27 19:09:08 +0100

    htbuilder

--- functions.py
+++ functions.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import tweepy
 import requests
+from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
+from htbuilder.units import percent, px
+from htbuilder.funcs import rgba, rgb
 
 def extract_all_tweets(username):
     user_tweets = st.session_state['api'].user_timeline(screen_name=username, 
@@ -97,3 +100,78 @@ def theTweet(tweet_url):
 	response = requests.get(api)
 	res = response.json()["html"]
 	return res
+
+
+def image(src_as_string, **style):
+    return img(src=src_as_string, style=styles(**style), width=150)
+
+
+def link(link, text, **style):
+    return a(_href=link, _target="_blank", style=styles(**style))(text)
+
+
+def layout(*args):
+
+    style = """
+    <style>
+      MainMenu {visibility: hidden;}
+      footer {visibility: hidden;}
+     .stApp { bottom: 105px; }
+     img{
+        background-color: #ddd;
+     }
+     img:hover{
+        background-color: rgba(59,130,246,.5);
+     }
+    </style>
+    """
+
+    style_div = styles(
+        position="fixed",
+        left=0,
+        bottom=0,
+        margin=px(0, 0, 0, 0),
+        width=percent(100),
+        color="black",
+        text_align="center",
+        height="auto",
+        opacity=1
+    )
+
+    style_hr = styles(
+        # display="block",
+        # margin=px(8, 8, "auto", "auto"),
+        # border_style="inset",
+        # border_width=px(2)
+    )
+
+    body = p()
+    foot = div(
+        style=style_div
+    )(
+        hr(
+            style=style_hr
+        ),
+        body
+    )
+
+    st.markdown(style, unsafe_allow_html=True)
+
+    for arg in args:
+        if isinstance(arg, str):
+            body(arg)
+
+        elif isinstance(arg, HtmlElement):
+            body(arg)
+
+    st.markdown(str(foot), unsafe_allow_html=True)
+
+
+def footer():
+    myargs = [
+        "Made by ",
+        link("https://twitter.com/nainia_ayoub", "@nainia_ayoub"),
+        br(),
+        link("https://buymeacoffee.com/nainiayoub", image('https://cdn.buymeacoffee.com/assets/img/home-page-v3/bmc-new-logo.png')),
+    ]
+    layout(*myargs)
